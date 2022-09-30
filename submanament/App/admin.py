632a2a63454ad7e004e021505ee9e6c805ecbd33
Ankush Rathod <111727320+Ankushrathod123@users.noreply.github.com>
2022-09-30 12:40:04 +0530

    Add files via upload

---
+++ submanament/App/admin.py
@@ -0,0 +1,7 @@
+from binascii import Incomplete
+from django.contrib import admin
+from .models import Employee,Role,Department
+# Register your models here.
+admin.site.register(Employee)
+admin.site.register(Role)
+admin.site.register(Department)
